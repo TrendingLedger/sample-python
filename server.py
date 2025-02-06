e1b99fd231d0e8313c42cfa973f51f385ce365d5
Trending Ledger <tomq1031@gmail.com>
2025-02-06 10:54:30 -0500

    testing python

--- server.py
+++ server.py
@@ -1,16 +1,21 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 from http import HTTPStatus
 
-# Create FastAPI app instance
 app = FastAPI()
 
-# Define the GET endpoint
+SECRET_CODE = "98765123450"  # Secret code to validate
+
 @app.get("/{path_param}", response_class=HTMLResponse)
-def read_root(path_param: str):
-    msg = f'Hello WORLD CHANGES! You requested the path: {path_param}'
-    return HTMLResponse(content=msg, status_code=HTTPStatus.OK)
+def read_item(path_param: str, code: str = None):
+    # Check if the provided code is valid
+    if code == SECRET_CODE:
+        msg = f'Hello WORLD CHANGES! You requested the path: {path_param}'
+        return HTMLResponse(content=msg, status_code=HTTPStatus.OK)
+    else:
+        raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail="nothing much")
+
 
 # Set the port from the environment variable, defaulting to 80
 port = int(os.getenv('PORT', 80))
