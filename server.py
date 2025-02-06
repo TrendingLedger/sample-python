0fe79355a90b886dc7401835a0b1a90b83484d16
Trending Ledger <tomq1031@gmail.com>
2025-02-06 10:43:24 -0500

    hey whats up

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 import os
-import http.server
-import socketserver
-
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from http import HTTPStatus
 
+# Create FastAPI app instance
+app = FastAPI()
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello WORLD CHANGES! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+# Define the GET endpoint
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    msg = f'Hello WORLD CHANGES! you requested {str(app)}'  # Similar to your original message
+    return HTMLResponse(content=msg, status_code=HTTPStatus.OK)
 
-
+# Set the port from the environment variable, defaulting to 80
 port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+
+# Run the app using uvicorn (for local testing)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
